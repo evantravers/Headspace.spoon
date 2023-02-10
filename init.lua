bac78ceadac31128ae407d45b3d9e85a2f00dce1
Evan Travers <evantravers@gmail.com>
2023-02-10 10:38:37 -0600

    feat: move away from spaces
    Instead of spaces, we can now send a list of tags to block.

--- init.lua
+++ init.lua
@@ -19,47 +19,13 @@
 ---
 --- Optionally, you can define a setup function at config.spaces.<key> that is
 --- run when the space is started.
----
---- OPTIONS:
----
---- # Example:
----
---- config.spaces = {
----   text = "Example",
----   subText = "More about the example",
----   image = hs.image.imageFromAppBundle('foo.bar.com'),
----
----   funcs = "example",
----
----   launch = {"table", "of", "tags"},
----   blacklist = {"table", "of", "tags"},
----   == OR ==
----   whitelist = {"table", "of", "tags"}
----
----   intentRequired = true
----   intentSuggestions = {}
---- }
----
---- config.funcs.example = {
----   setup = function()
----     hs.urlevent.openURL("http://hammerspoon.org")
----   end
---- }
----
---- The goal is to get into another space, even when working from home.
----
---- Future expansions...
---- DND status?
---- Custom Desktop Background with prompts for focus, writing, code?
---- Musical cues?
 
 local m = {
   name = "Headspace",
-  version = "1.1",
+  version = "2.0",
   author = "Evan Travers <evantravers@gmail.com>",
   license = "MIT <https://opensource.org/licenses/MIT>",
   homepage = "https://github.com/evantravers/headspace.spoon",
-  tagged = {}
 }
 
 -- CONFIG ==============
@@ -84,21 +50,18 @@ local moduleStyle = fn.copy(hs.alert.defaultStyle)
 function m:start()
   m.watcher = hs.application.watcher.new(function(appName, event, hsapp)
     if event == hs.application.watcher.launched then
-      local appConfig = m.config.applications[hsapp:bundleID()]
-      local space = m.getSpace()
-
-      if not m.allowed(appConfig) then
+      if not m.allowed(hsapp) then
         hs.alert(
-          "🛑: " .. hsapp:name() .. "\n" ..
-          "📂: " .. space.text,
+          "🛑: " .. hsapp:name(),
           moduleStyle
         )
         hsapp:kill()
       end
     end
-    hs.urlevent.bind("switchSpace", m.switchSpace)
-    hs.urlevent.bind("stopSpace", m.stopSpace)
   end):start()
+
+  hs.urlevent.bind("blacklist", m.setBlacklist)
+  hs.urlevent.bind("whitelist", m.setWhitelist)
 
   return self
 end
@@ -115,131 +78,55 @@ function m:stop()
   -- kill any timers
   m.timer = nil
 
-  hs.urlevent.bind("switchSpace", nil) -- remove callback
-  hs.urlevent.bind("stopSpace", nil) -- remove callback
+  -- clear residual spaces
+  hs.settings.clear("headspace")
+
+  hs.urlevent.bind("setBlacklist", nil)
+  hs.urlevent.bind("setWhitelist", nil)
+
   return self
 end
 
---- Headspace:loadConfig(configTable) -> table
---- Method
---- Adds a device to USBObserver's watch list
----
---- Parameters:
----  * configTable - A table containing the spaces and applications.-
----
---- Returns:
----  * self
-function m:loadConfig(configTable)
-  -- FIXME: Do error checking when loaded? (should be applications, etc.)
-  m.config = configTable
-  m.computeTagged(m.config.applications)
-  return self
+m.getWhitelist = function()
+  return hs.settings.get("headspace")["whitelist"]
 end
 
--- HELPERS =============
-
--- Switching spaces
-
-m.setSpace = function(space)
-  hs.settings.set('headspace', space.text)
+m.getBlacklist = function()
+  return hs.settings.get("headspace")["blacklist"]
 end
 
-m.getSpace = function()
-  return hs.fnutils.find(m.config.spaces, function(space)
-    return hs.settings.get('headspace') == space.text
-  end)
-end
+m.allowed = function(app)
+  local tags = hs.fs.tagsGet(app:path())
 
-m.hasFunc = function(key, func)
-  return m.config.funcs[key] and m.config.funcs[key][func]
-end
+  if not tags then return true end
 
-m.switchSpace = function(_eventName, params)
-  local space = hs.fnutils.find(m.config.spaces, function(s)
-    return s.text:find(params["name"])
-  end)
-  m.switch(space)
-end
-
-m.stopSpace = function(_eventName, _params)
-  hs.settings.clear('headspace')
-  return nil
-end
-
-m.switch = function(space)
-  if space ~= nil then
-    m.setSpace(space)
-
-    -- launch / close apps
-    if space.launch then
-      m.tagsToBundleid(space.launch, function(bundleID)
-        hs.application.launchOrFocusByBundleID(bundleID)
-      end)
-    end
-
-    if space.blacklist then
-      m.tagsToBundleid(space.blacklist, function(bundleID)
-        local app = hs.application.get(bundleID)
-        if app then app:kill() end
-      end)
-    end
-
-    if space.whitelist then
-      fn.map(m.config.applications, function(appConfig)
-        if not m.allowed(appConfig) then
-          local app = hs.application.get(appConfig.bundleID)
-          if app then
-            app:kill()
-          end
-        end
-      end)
-    end
+  if fn.contains(tags, "whitelisted") then
+    return true
   end
-end
 
-m.computeTagged = function(listOfApplications)
-  fn.map(listOfApplications, function(appConfig)
-    if appConfig.tags then
-      fn.map(appConfig.tags, function(tag)
-        if not m.tagged[tag] then m.tagged[tag] = {} end
-        table.insert(m.tagged[tag], appConfig.bundleID)
-      end)
-    end
-  end)
-end
-
-m.appsTaggedWith = function(tag)
-  return m.tagged[tag]
-end
-
-m.tagsToBundleid = function(listOfTags, func)
-  fn.map(listOfTags, function(tag)
-    fn.map(m.appsTaggedWith(tag), function(appConfig)
-      func(appConfig)
+  if m.getWhitelist then
+    return fn.some(m.getWhitelist, function(tag)
+      return fn.contains(tags, tag)
     end)
-  end)
-end
-
-m.allowed = function(appConfig)
-  if appConfig and appConfig.tags then
-    if appConfig.whitelisted then
-      return true
-    else
-      local space = m.getSpace()
-      if space.whitelist then
-        return fn.some(space.whitelist, function(tag)
-          return fn.contains(m.tagged[tag], appConfig.bundleID)
-        end)
-      else
-        if space.blacklist then
-          return fn.every(space.blacklist, function(tag)
-            return not fn.contains(appConfig.tags, tag)
-          end)
-        end
-      end
+  else
+    if m.getBlacklist then
+      return fn.every(m.getBlacklist, function(tag)
+        return not fn.contains(tags, tag)
+      end)
     end
   end
+
   return true
+end
+
+function m.setBlacklist(_eventName, params)
+  local l = fn.split(params["tags"])
+  hs.settings.set("headspace", { ["blacklist"] = l })
+end
+
+function m.setWhitelist(_eventName, params)
+  local l = fn.split(params["tags"])
+  hs.settings.set("headspace", { ["whitelist"] = l })
 end
 
 return m
